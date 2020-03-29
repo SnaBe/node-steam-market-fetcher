d89a5270d10f63cc9430d854e33c23b1333d1f25
Simon <fynboen1999@gmail.com>
2020-03-29 17:25:01 +0200

    Core project files

---
+++ bin/util.js
@@ -0,0 +1,43 @@
+//Import pre-written resources 
+const currency = require('../resources/currency');
+const data = require('../resources/data');
+
+/**
+ * Set the currency for a new instance based on the parsed currency option
+ * @param {Object} option any currency code used by the Steamcommunity market, as a String
+ * @returns {Number} a Number representing the currency code
+ */
+function setCurrency (option) {
+    //Return the currency code as a number
+    return (Object.values(currency).includes(option)) ? option : currency['USD'];
+}
+
+/**
+ * Set the data format for a new instance based on the parsed data option
+ * @param {String} option any data format accepted by Steam, as a String
+ * @returns {String} a String representing the data format
+ */
+function setDataFormat (option) {
+    //Return the data format String
+    return (data.includes(option)) ? option : data[0];
+}
+
+/**
+ * Resize a Steamcommunity market listing url to a preferred image size
+ * @param {String} image_url a default Steamcommunity market listing image
+ * @param {Number} px the size of the image in pixels
+ * @returns {String} the resized image url
+ */
+function resizeImage (image_url, px) {
+    //Check if the parsed image url contains the default item listing size
+    //If true, replace the image urls default size with the preferred size
+    //If false, return the original image url
+    return (image_url.includes('62fx62f')) ? image_url.replace('62fx62f', `${px}fx${px}f`) : image_url;
+}
+
+//Export the functions
+module.exports = {
+    setCurrency,
+    setDataFormat,
+    resizeImage
+}
---
+++ resources/data.js
@@ -0,0 +1,5 @@
+module.exports = [
+    'json',
+    'vdf',
+    'xml'
+];
