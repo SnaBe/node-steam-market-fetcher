59325f6052ff9b9a7bff3e11081cd5bb6cca0726
SnaBe <fynboen1999@gmail.com>
2023-03-05 20:03:31 +0100

    Retry styles

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,7 +19,7 @@ module.exports = {
         ],
         "linebreak-style": [
             "error",
-            "windows"
+            (process.platform === "win32" ? "windows" : "unix")
         ],
         "quotes": [
             "error",
