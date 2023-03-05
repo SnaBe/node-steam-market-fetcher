f74d35619572249280348286a15ba07428a85259
SnaBe <fynboen1999@gmail.com>
2023-03-05 19:37:43 +0100

    Updated ESLint config

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+    "env": {
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4,
+            {
+                "SwitchCase": 1
+            }
+        ],
+        "linebreak-style": [
+            "error",
+            process.platform === "win32" ? "windows" : "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ],
+        "@typescript-eslint/no-explicit-any": [
+            "off"
+        ]
+    }
+}
