70c9a0863f176b29abc110852c99fbc7e0014098
SnaBe <fynboen1999@gmail.com>
2023-04-03 23:49:13 +0200

    Add comments for resources

--- resources/ESteamCurrency.js
+++ resources/ESteamCurrency.js
@@ -1,43 +1,43 @@
 // Currencies used by the Steam Community Market
 // USD is the default option
 module.exports = {
-    'USD': 1,
-    'GBP': 2,
-    'EUR': 3,
-    'CHF': 4,
-    'RUB': 5,
-    'PLN': 6,
-    'BRL': 7,
-    'JPY': 8,
-    'SEK': 9,
-    'IDR': 10,
-    'MYR': 11,
-    'PHP': 12,
-    'SGD': 13,
-    'THB': 14,
-    'VND': 15,
-    'KRW': 16,
-    'TRY': 17,
-    'UAH': 18,
-    'MXN': 19,
-    'CAD': 20,
-    'AUD': 21,
-    'NZD': 22,
-    'CNY': 23,
-    'INR': 24,
-    'CLP': 25,
-    'PEN': 26,
-    'COP': 27,
-    'ZAR': 28,
-    'HKD': 29,
-    'TWD': 30,
-    'SRD': 31,
-    'AED': 32,
-    'ARS': 34,
-    'ILS': 35,
-    'KZT': 37,
-    'KWD': 38,
-    'QAR': 39,
-    'CRC': 40,
-    'UYU': 41
+    'USD': 1, // United States Dollar
+    'GBP': 2, // Pound Sterling
+    'EUR': 3, // Euro
+    'CHF': 4, // Switzerland Franc
+    'RUB': 5, // Russian Ruble
+    'PLN': 6, // Polish Zloty
+    'BRL': 7, // Brazilian Real
+    'JPY': 8, // Japanese yen
+    'SEK': 9, // Swedish krona
+    'IDR': 10, // Indonesian Rupiah
+    'MYR': 11, // Malaysian Ringgit
+    'PHP': 12, // Philippine peso
+    'SGD': 13, // Singapore Dollar
+    'THB': 14, // Thailand Baht
+    'VND': 15, // Vietnamese Dong
+    'KRW': 16, // South Korean Won
+    'TRY': 17, // Turkish lira
+    'UAH': 18, // Ukrainian hryvnia
+    'MXN': 19, // Mexican Peso
+    'CAD': 20, // Canadian dollar
+    'AUD': 21, // Australian Dollar
+    'NZD': 22, // New Zealand Dollar
+    'CNY': 23, // Chinese Yuan Renminbi
+    'INR': 24, // Indian Rupee
+    'CLP': 25, // Chilean Peso
+    'PEN': 26, // Peruvian Sol
+    'COP': 27, // Colombian Peso
+    'ZAR': 28, // South Africa Rand
+    'HKD': 29, // Hong Kong Dollar
+    'TWD': 30, // Taiwan New Dollar
+    'SRD': 31, // Surinamese Dollar
+    'AED': 32, // United Arab Emirates dirham
+    'ARS': 34, // Argentine Peso
+    'ILS': 35, // Israeli Shekel
+    'KZT': 37, // Kazakhstani Tenge
+    'KWD': 38, // Kuwaiti dina
+    'QAR': 39, // Qatari riyal
+    'CRC': 40, // Costa Rican colón
+    'UYU': 41 // Uruguayan Peso
 }
--- resources/ESteamDataFormat.js
+++ resources/ESteamDataFormat.js
@@ -1,7 +1,7 @@
 // Data formats accepted by the Steam Community API
 // JSON is the default option
 module.exports = [
-    'json',
-    'vdf',
-    'xml'
+    'json', // JavaScript Object Notation
+    'vdf', // A file format used by Valve's Source game engine
+    'xml' // Extensible Markup Language
 ]
